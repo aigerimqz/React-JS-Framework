52a35743f52bd8ca65f11441e53f489e44e3e7fa
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-09-26 02:53:11 +0500

    changes into about component

--- Lab2/react-project/src/components/AboutUs/About.jsx
+++ Lab2/react-project/src/components/AboutUs/About.jsx
@@ -6,10 +6,10 @@ export default function About(){
     
     <div className="about__section">
         <h1 className="about__title">Enjoy your best travel ever</h1>
-        <p className="about__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quibusdam molestias ipsam nesciunt laborum eum, vel blanditiis fuga saepe modi, pariatur mollitia cum? Eos dolor beatae aliquid incidunt recusandae excepturi!</p>
+        <p className="about__text"><h1>About Us</h1> <br /> <hr className="about__line" /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quibusdam molestias ipsam nesciunt laborum eum, vel blanditiis fuga saepe modi, pariatur mollitia cum? Eos dolor beatae aliquid incidunt recusandae excepturi!</p>
     </div>
-    <div className="team__section">
-        <h1>Meet the team</h1>
+    <div className="info__section">
+        
         <ContactItem></ContactItem>
     </div>
   
