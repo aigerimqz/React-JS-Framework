4b036f2f4977a1c025562c1262e4165f52542a6c
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-09-26 04:28:57 +0500

    imported social media icons to contact section

--- Lab2/react-project/src/components/AboutUs/About.jsx
+++ Lab2/react-project/src/components/AboutUs/About.jsx
@@ -1,5 +1,8 @@
 import "./About.css"
-import ContactItem from "./ContactItem"
+import instaIcon from "../../assets/icons/insta.png"
+import facebookIcon from "../../assets/icons/facebook.png"
+import telegramIcon from "../../assets/icons/telegram.png"
+import tiktokIcon from "../../assets/icons/tiktok.png"
 export default function About(){
     return (
     <>
@@ -8,9 +11,15 @@ export default function About(){
         <h1 className="about__title">Enjoy your best travel ever</h1>
         <p className="about__text"><h1>About Us</h1> <br /> <hr className="about__line" /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quibusdam molestias ipsam nesciunt laborum eum, vel blanditiis fuga saepe modi, pariatur mollitia cum? Eos dolor beatae aliquid incidunt recusandae excepturi!</p>
     </div>
-    <div className="info__section">
+    <div className="contact__section">
+        <li>
+            <a href="https:/instagram.com"><img src={instaIcon} alt="instagram" /></a>
+            <a href="https:/www."><img src={facebookIcon} alt="facebook" /></a>
+            <a href=""><img src={telegramIcon} alt="telegram" /></a>
+            <a href=""><img src={tiktokIcon} alt="tiktok" /></a>
+        </li>
         
-        <ContactItem></ContactItem>
+        
     </div>
   
    
