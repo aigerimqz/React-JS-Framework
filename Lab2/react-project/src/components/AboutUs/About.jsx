018647c42a5562ff825770b92bf02e0a2f87fea3
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-09-26 04:58:16 +0500

    finished contacts part

--- Lab2/react-project/src/components/AboutUs/About.jsx
+++ Lab2/react-project/src/components/AboutUs/About.jsx
@@ -9,16 +9,17 @@ export default function About(){
     
     <div className="about__section">
         <h1 className="about__title">Enjoy your best travel ever</h1>
-        <p className="about__text"><h1>About Us</h1> <br /> <hr className="about__line" /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quibusdam molestias ipsam nesciunt laborum eum, vel blanditiis fuga saepe modi, pariatur mollitia cum? Eos dolor beatae aliquid incidunt recusandae excepturi!</p>
+        <p className="about__text"><h1>About Us</h1> <br />  Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quibusdam molestias ipsam nesciunt laborum eum, vel blanditiis fuga saepe modi, pariatur mollitia cum? Eos dolor beatae aliquid incidunt recusandae excepturi!</p>
     </div>
     <div className="contact__section">
+        <hr className="contact__line"/>
         <li>
             <a className="contact__item" href="https:/instagram.com"><img src={instaIcon} alt="instagram" /></a>
             <a className="contact__item" href="https:/www."><img src={facebookIcon} alt="facebook" /></a>
             <a className="contact__item" href=""><img src={telegramIcon} alt="telegram" /></a>
             <a className="contact__item" href=""><img src={tiktokIcon} alt="tiktok" /></a>
         </li>
-        
+        <hr className="contact__line"/>
         
     </div>
   
