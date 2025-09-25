a1798823fdeb1f9c5759c29680f54719d75cebbd
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-09-25 17:42:28 +0500

    added header section just optional and styled it, created contact component

--- Lab2/react-project/src/App.jsx
+++ Lab2/react-project/src/App.jsx
@@ -1,12 +1,23 @@
 import { useState } from 'react'
-import About from './About'
+import About from './components/AboutUs/About'
 import './App.css'
 
 function App() {
   
   return (
     <>
-    <About></About>
+    <div className="container">
+      <div className="header__section">
+        <h1 className='header__title'>WebSite</h1>
+        <div className="header__navigation">
+          <a href="#">About Us</a>
+          <a href="#">Home</a>
+          <a href="#">Blog</a>
+        </div>
+      </div>
+     <About></About>
+    </div>
+   
     </>
   )
 }
