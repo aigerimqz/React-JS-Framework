4b62ddfb96cc9f92c8f3eb02a84523f25ef0f923
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-09-26 06:37:30 +0500

    changed color for about section

--- Lab2/react-project/src/App.jsx
+++ Lab2/react-project/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import About from './components/AboutUs/About'
-import Home from './components/Home/Home'
-import './App.css'
+import { useState } from "react";
+import About from "./components/AboutUs/About";
+import Home from "./components/Home/Home";
+import "./App.css";
 
 function App() {
-  
   return (
     <>
-    <div className="section">
-      <div className="container">
-        <div className="header__section">
-          <h1 className='header__title'>WebSite</h1>
-          <div className="header__navigation">
-            <a href="#">About Us</a>
-            <a href="#">Home</a>
-            <a href="#">Blog</a>
+      <div className="section">
+        <div className="container">
+          <div className="header__section">
+            <h1 className="header__title">WebSite</h1>
+            <div className="header__navigation">
+              <a href="#">About Us</a>
+              <a href="#">Home</a>
+              <a href="#">Blog</a>
+            </div>
           </div>
+          <Home></Home>
         </div>
-        <Home></Home>
-      
       </div>
-
-    </div>
-    <div className="container">
-      <About></About>
-    </div>
-    
-    
-   
+      <div className="container">
+        <About></About>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
