3a0df35469143225b1f8f3a6209ef65c27bc270c
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-10-15 19:21:03 +0500

    added folders to lab4

---
+++ Lab4/react-project/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import About from "./components/AboutUs/About";
+import Home from "./components/Home/Home";
+import TourList from "./components/Tours/TourList";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="section">
+        <div className="container">
+          <div className="header__section">
+            <h1 className="header__title">WebSite</h1>
+            <div className="header__navigation">
+              <a href="#">Home</a>
+              <a href="#">About</a>
+              <a href="#">Blog</a>
+            </div>
+          </div>
+          <Home></Home>
+        </div>
+      </div>
+      <div className="container">
+        <About></About>
+        <TourList></TourList>
+      </div>
+      
+
+    </>
+
+  );
+}
+
+export default App;
---
+++ Lab4/react-project/src/components/Tours/TourItem.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./TourItem.css";
+
+
+
+export default function TourItem({tour}){
+  return (
+    <div className="tour-card">
+        
+      <img src={tour.photo} alt={tour.name} className="tour-image" />
+      <div className="tour-content">
+        <h2 className="tour-title">{tour.name}</h2>
+        <p className="tour-country">{tour.country}</p>
+        <p className="tour-description">{tour.description}</p>
+        <p className="tour-price">{tour.price.toLocaleString()} ₸</p>
+      </div>
+    </div>
+  );
+
+}
