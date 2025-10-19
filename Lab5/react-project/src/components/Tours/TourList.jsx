021b8d65542e9880f6d411825b189f4e9dd36370
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-10-19 22:38:54 +0500

    added lab5 folders

---
+++ Lab5/react-project/src/components/Tours/TourList.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import TourItem from "./TourItem";
+import "./TourList.css";
+
+
+export default function TourList(){
+  const [items, setItems] = useState([]);
+
+  async function load() {
+    const result = await fetch(
+      "http://127.0.0.1:8000/api/tours/"
+    );
+    const data = await result.json();
+    console.log(data, "fetched data");
+    setItems(data);
+  }
+  return (
+    <div className="tour__list">
+      <h2 className="tour__title">Show tours</h2>
+      <button className="loadBtn" onClick={load}>Load tours</button>
+      
+      {items ? 
+        <ul className="tour__items">
+        {items.map((item) => (
+          // <li key={item.id}>{item.name}</li>
+          <TourItem key={item.id} tour = {item}></TourItem>
+        ))}
+        </ul>
+      : <p>None</p>}
+      
+    </div>
+  )
+}
