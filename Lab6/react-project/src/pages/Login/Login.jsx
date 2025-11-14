e7b1d45ddbae557adbef418fd4ef2e63c0deac36
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-11-14 13:48:04 +0500

    changes added

--- Lab6/react-project/src/pages/Login/Login.jsx
+++ Lab6/react-project/src/pages/Login/Login.jsx
@@ -2,6 +2,7 @@ import "../Login/Login.css";
 export default function Login(){
     return (
         <>
+        <h1><center>Auth to be added soon.</center></h1>
         <div className="login__block">
             <input className="input input__email" type="email" name="" placeholder="Email" id="" />
             <input className="input input__password" type="password" name="" placeholder="Password" id="" />
