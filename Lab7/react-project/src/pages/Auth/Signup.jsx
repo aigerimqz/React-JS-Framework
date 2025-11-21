552e365eb636d790a5bad2c30b76b9d3d5565198
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-11-21 13:02:42 +0500

    added confirm password part

--- Lab7/react-project/src/pages/Auth/Signup.jsx
+++ Lab7/react-project/src/pages/Auth/Signup.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import "./Login.css"; // можно использовать тот же CSS, что для Login
+import "./Login.css"; 
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase";
 
@@ -8,11 +8,16 @@ export default function Signup() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
     const [loading, setLoading] = useState(false);
 
     const navigate = useNavigate();
 
     const handleSignup = async () => {
+        if (password !== confirmPassword) {
+            setError("Passwords do not match");
+            return;
+        }
         try {
             setError("");
             setLoading(true);
@@ -29,7 +34,7 @@ export default function Signup() {
             <h1 style={{ textAlign: "center" }}>Sign Up</h1>
 
             <form className="login__block">
-                {error && <p className="error">{error}</p>}
+                
 
                 <input 
                     className="input input__email"
@@ -48,7 +53,15 @@ export default function Signup() {
                     onChange={(e) => setPassword(e.target.value)}
                     required
                 />
-
+                <input 
+                    className="input input__password"
+                    type="password"
+                    placeholder="Confirm Password"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    required
+                />
+                {error && <p className="error">{error}</p>}
                 <button
                     type="button"
                     className="login__btn"
