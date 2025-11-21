c7f556d1b704fb611818281aaaa615737dfdcac5
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-11-21 12:05:08 +0500

    added signup and profile component

--- Lab7/react-project/src/pages/Auth/Signup.jsx
+++ Lab7/react-project/src/pages/Auth/Signup.jsx
@@ -1,3 +1,38 @@
-export default function Signup(){
-    
+import { useState } from "react";
+// import { createUserWithEmailAndPassword } from "./firebase/auth";
+
+import { useNavigate, Link } from "react-router-dom";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
+import "./Login.css";
+export default function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleSignup = async () => {
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      navigate("/profile");
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  return (
+    <div className="signup__block">
+      <h1>Signup</h1>
+
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
+      <input className="input input__email" type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+      <input className="input input__password" type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+      <input className="input input__confirm" type="password" placeholder="Confirm a password" onChange={(e) => setError(e.target.value.includes())}/>
+
+      <button onClick={handleSignup}>Signup</button>
+
+      <p>Have an account? <Link to="/login">Login</Link></p>
+    </div>
+  );
 }
---
+++ Lab7/react-project/src/pages/Profile/Profile.jsx
@@ -0,0 +1,19 @@
+
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+
+export default function Profile() {
+  const { user, logout } = useAuth;
+
+  if (!user) return <Navigate to="/login" />;
+
+  return (
+    <div>
+      <h1>Profile</h1>
+      <p>Email: {user.email}</p>
+      <p>UID: {user.uid}</p>
+
+      <button onClick={logout}>Logout</button>
+    </div>
+  );
+}
