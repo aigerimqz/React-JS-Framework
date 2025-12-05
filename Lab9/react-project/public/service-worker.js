5554e758d3a9ba30d7b775e1b29d9fa0075fb234
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-12-05 05:09:44 +0500

    add install and activate eventlisteneres

--- Lab9/react-project/public/service-worker.js
+++ Lab9/react-project/public/service-worker.js
@@ -10,5 +10,33 @@ const APP_SHELL = [
     "/icons/icon-512.png",
 ];
 
+self.addEventListener("install", (event) => {
+    console.log("[SW] Install event");
+
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.add.addAll(APP_SHELL);
+
+        })
+    )
+})
 
 
+
+
+
+self.addEventListener("activate", (event) => {
+  console.log("[SW] Activate event");
+
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME && key !== API_CACHE)
+          .map((key) => caches.delete(key))
+      )
+    )
+  )
+
+  self.clients.claim();
+})
