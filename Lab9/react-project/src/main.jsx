3ca08260bad3bcc952cec510ea6855976ad8a4b1
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-12-05 05:17:00 +0500

    connect SW into main.jsx

--- Lab9/react-project/src/main.jsx
+++ Lab9/react-project/src/main.jsx
@@ -16,3 +16,10 @@ createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </Provider>
 )
+
+
+if("serviceWorker" in navigator){
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js").then(() => console.log("Service Worker registered")).catch((err) => console.log("SW registration failed", err));
+  });
+}
