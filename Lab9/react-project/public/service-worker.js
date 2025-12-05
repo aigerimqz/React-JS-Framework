6b85ed95186a1c797112309690a4113f99550568
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-12-05 05:07:27 +0500

    create service-worker and add app shell to service-worker

---
+++ Lab9/react-project/public/service-worker.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = "app-shell-v1";
+const API_CACHE = "api-cache-v1";
+
+
+const APP_SHELL = [
+    "/",
+    "../index.html",
+    "/manifest.json",
+    "/icons/icon-192.png",
+    "/icons/icon-512.png",
+];
+
+
+
